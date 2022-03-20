974201d383d2939affc82302e2753ceed077626f
walkor <walkor@workerman.net>
2022-03-20 22:57:48 +0800

    contributors

---
+++ script/contributors.php
@@ -0,0 +1,72 @@
+<?php
+ini_set('display_errors', 'on');
+error_reporting(E_ALL);
+
+$opts = array(
+    'http'=>array(
+        'header'=>"User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36\r\n"
+    )
+);
+$context = stream_context_create($opts);
+
+$webman_contributors = file_get_contents('https://api.github.com/repos/walkor/webman/contributors', false, $context);
+if (!$webman_contributors) die;
+$webman_contributors = json_decode($webman_contributors, true);
+if (!$webman_contributors) die;
+
+$manual_contributors = file_get_contents('https://api.github.com/repos/webman-php/webman-manual/contributors', false, $context);
+if (!$manual_contributors) die;
+$manual_contributors = json_decode($manual_contributors, true);
+if (!$manual_contributors) die;
+
+$contributors = [];
+foreach ($webman_contributors as $contributor) {
+    if ($contributor['login'] == 'walkor') continue;
+    $contributors[$contributor['id']] = [
+        'name' => $contributor['login'],
+        'avatar' => $contributor['avatar_url'],
+        'contributions' => $contributor['contributions']
+    ];
+}
+
+foreach ($manual_contributors as $contributor) {
+    if ($contributor['login'] == 'walkor') continue;
+    $id = $contributor['id'];
+    if (!isset($contributors[$id])) {
+        $contributors[$id] = [
+            'name' => $contributor['login'],
+            'avatar' => $contributor['avatar_url'],
+            'contributions' => 0
+        ];
+    }
+    $contributors[$id]['contributions'] += $contributor['contributions'];
+}
+
+$sort = array_column($contributors, 'contributions');
+array_multisort($sort, SORT_DESC, SORT_NUMERIC, $contributors);
+
+$contributors_chunk = array_chunk($contributors, 8);
+
+$table = '<table>';
+foreach ($contributors_chunk as $contributors) {
+    $table .= '
+  <tr>';
+    foreach ($contributors as $contributor) {
+        $table .='
+    <td align="center">
+      <a href="https://github.com/'.$contributor['name'].'">
+        <img src="'.$contributor['avatar'].'" width="100px;"/><h5>'.$contributor['name'].'</h3>
+      </a>
+    </td>';
+    }
+    $table .= '
+  </tr>';
+}
+$table .= '
+</table>';
+
+$zh_cn_thanks = '#感谢以下贡献者对webman及文档的贡献';
+$en_thanks = '#Thank the following contributors for their contributions to webman and the document';
+
+file_put_contents(__DIR__ . '/../resource/doc/zh-cn/thanks.md', "$zh_cn_thanks\n$table\n");
+file_put_contents(__DIR__ . '/../resource/doc/en/thanks.md', "$en_thanks\n$table\n");
